fdb0eefc373dc44c0ac0f2ddb0293ece00d80fc4
luiz filho <luiz.filho@mutantbr.com>
2020-07-29 00:07:18 -0300

    comportamento spa

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 import './Menu.css'
 import Button from '../Button'
@@ -7,10 +8,10 @@ import Button from '../Button'
 function Menu () {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link href="/">
                 <img className="Logo" src={Logo} alt="LuizFlilho" />
-            </a>
-            <Button as="a" href="/">Novo Vídeo</Button>
+            </Link>
+            <Button as={Link} to="/cadastro/videos">Novo Vídeo</Button>
         </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Home from './pages/Home'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video'
+
+const Pagina404 = function () {
+  return (
+    <div>
+      Página 404
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/videos" component={CadastroVideo} />
+      <Route path="/" component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
